6155372a9611eadfde50fb10d748fb970b7d9d9a
nguyvinc <nguyvinc@oregonstate.edu>
2019-03-16 22:08:49 -0700

    Added ability to limit number of rows displayed

--- Code/server/public/scripts/clientRuns.js
+++ Code/server/public/scripts/clientRuns.js
@@ -5,16 +5,34 @@ function loadRunData() {
 		type: $("#sensorType").val()
 	}, (entries) => {
 		let data = "";					//String to hold table information
+		let limit = parseInt($("#dataLimit").val());
+		let counter = 0;
 		if(entries.length > 0) {		//If entries were found
-			$(entries).each((index, obj) => {		//For each entry, add it to the table string
-				data += "<tr>" +
-							"<td>"+obj.bot+"</td>" +
-							"<td>"+obj.name+"</td>" +
-							"<td>"+obj.time+"</td>" +
-							"<td>"+obj.type+"</td>" +
-							"<td>"+obj.value+"</td>" +
-						"</tr>";
-			});
+			if(Number.isNaN(limit)) {			//If no limit was specified, display all rows
+				$(entries).each((index, obj) => {		//For each entry, add it to the table string
+					data += "<tr>" +
+								"<td>"+obj.bot+"</td>" +
+								"<td>"+obj.name+"</td>" +
+								"<td>"+obj.time+"</td>" +
+								"<td>"+obj.type+"</td>" +
+								"<td>"+obj.value+"</td>" +
+							"</tr>";
+				});
+			}
+			else {						//If a limit was specified
+				$(entries).each((index, obj) => {		//For each entry
+					if(counter < limit){				//Add each entry to the table if the limit was not reached
+						data += "<tr>" +
+									"<td>"+obj.bot+"</td>" +
+									"<td>"+obj.name+"</td>" +
+									"<td>"+obj.time+"</td>" +
+									"<td>"+obj.type+"</td>" +
+									"<td>"+obj.value+"</td>" +
+								"</tr>";
+					}
+					counter ++;							//Increment the counter
+				});
+			}
 		} else {						//Else if no entries were found, notify the user
 			data += "<tr>" +
 						"<td colspan=5>No Data Found</td>" +
@@ -28,4 +46,9 @@ function loadRunData() {
 
 $(document).ready(() => {
 	$("#runSearch").on("click", loadRunData);	//Search on button click
+	
+	$("#botName, #runName, #sensorType, #dataLimit").on("keypress", function(e){	//Search when enter is pressed
+		if(e.which == 13)
+			loadRunData();
+	});
 });
